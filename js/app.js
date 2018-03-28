b079fd10cc7f481198a0859f40c953ec64d285d1
Ahmed Raed Moukhtar <ahmedraedm@outlook.com>
2018-03-28 08:02:00 +0200

    Add timer

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ function initialize() {
     $('.stars').children('li').children('i').eq(i).attr('class','fa fa-star') // remove any class from the last game except 'fa' class
   }
   $('.game-end').removeClass('game-end-show');
+  clear(); // Reset timer
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -121,7 +122,12 @@ function checkIfWin(){
   if (totMatchedCtr===16) { // All cards matched. Win!
     $('.game-end').addClass('game-end-show'); // show the celebrate background
     setTimeout(function(){  // Wait untill the celebration background appears
-      let answer =confirm(`Congratulations! You won, your score is: ${counter} and stars: ${stars}
+      let timerScore = h1.textContent;
+      let answer =confirm(`Congratulations! You won, your score is:
+
+Counter: ${counter}
+Stars: ${stars}
+Timer: ${timerScore}
 Play again?`);
   if (answer) {
     initialize();
@@ -146,3 +152,35 @@ $('li').on('click', function (evt) {
 $('.fa-repeat').on('click', function () {
   initialize();
 })
+
+// Timer
+
+var h1 = document.getElementsByTagName('h1')[1],
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+/* Clear button */
+function clear() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
